380bdf1f23c2e51e2916f150ccf32876f3d3b77d
Jibon Krishna Roy <mailme.jibon@gmail.com>
2025-04-07 10:05:57 +0600

    dynamicApi comment update

--- src/redux/api/dynamic/dynamicApi.ts
+++ src/redux/api/dynamic/dynamicApi.ts
@@ -2,12 +2,15 @@ import { baseApi } from '../baseApi';
 
 type DynamicApiParams = {
   endpoint: string; // API endpoint
+  // remove if do not needed the disabled explicit any
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   data?: any; // Request body for POST/PUT requests
   method?: 'GET' | 'POST' | 'PUT' | 'DELETE' | string; // Dynamic HTTP methods
   headers?: Record<string, string>; // Custom headers
+  // remove if do not needed the disabled explicit any
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   invalidatesTags?: { type: any; id?: string | number }[]; // Cache invalidation tags
+  // remove if do not needed the disabled explicit any
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   providesTags?: { type: any; id?: string | number }[]; // Cache provision tags
 };
